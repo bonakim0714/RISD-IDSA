0463e8a01c4f632acbc7f6095cfc489c676657f1
bonakim0714 <bkim12@risd.edu>
2018-01-10 15:56:28 -0500

    Update sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -1,20 +1,22 @@
 function setup() {
- createCanvas(windowWidth, windowHeight);
- background('#101010')
+	createCanvas(windowWidth, windowHeight);
+	background('#101010');
+
+}
+
+function draw() {
+
+
+	if (mouseIsPressed) {
+		fill(random(255),random(255),random(255),50)
+	} else{
+		fill('#101010')
+		stroke(255,80);
+	}
+	ellipse(mouseX, mouseY, 120, 120);
 }
 
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight,[noRedraw]);
-}
-
-
-function draw() {
-  if (mouseIsPressed) {
-    fill(random(255),random(255),random(255),50)
-  } else {
-    fill('#101010');
-    stroke(255,80);
-  }
-  ellipse(mouseX, mouseY, 130,130);
 }
