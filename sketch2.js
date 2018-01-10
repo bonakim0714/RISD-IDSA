fb3c38703135e1f7cd2053c3215d1a8a93cbc36d
bonakim0714 <bkim12@risd.edu>
2018-01-10 16:39:29 -0500

    mouseclick

--- sketch2.js
+++ sketch2.js
@@ -1,21 +1,35 @@
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background('#101010');
-
 }
+var style = 0;
 
 function draw() {
+  fill('#101010');
+  stroke(255,80);;
 
 
-	if (mouseIsPressed) {
-		fill(random(255),random(255),random(255),50)
-	} else{
-		fill('#101010')
-		stroke(255,80);
-	}
-	ellipse(mouseX, mouseY, 120, 120);
+	// if (mouseIsPressed) {
+	// 	fill(random(255),random(255),random(255),50)
+	// } else{
+	// 	fill('#101010')
+	// 	stroke(255,80);
+	// }
+
+  if (style == 1){
+     fill(random(255),random(255),random(255),50);
+   }
+
+  ellipse(mouseX, mouseY, 120, 120);
 }
 
+function mouseClicked(){
+  if(style == 0){
+    style = 1;
+  } else if (style == 1){
+    style = 0; //makes it not so colorful
+  }
+}
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight,[noRedraw]);
